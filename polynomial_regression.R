553ae436c5e3bea97353049c39a7ad1e363a2164
Juan Manuel de Palacios <juanma36@gmail.com>
2020-01-26 21:05:00 -0300

    regresión lineal multiple

--- polynomial_regression.R
+++ polynomial_regression.R
@@ -1,78 +1,67 @@
-# Polynomial Regression
+#Regresión Polinómica
 
-# Importing the dataset
+#Importar el dataset
 dataset = read.csv('Position_Salaries.csv')
-dataset = dataset[2:3]
+dataset = dataset[, 2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
+
+#Dividir los datos en conjunto de entrenamiento y conjunto de test
+#install.packages("caTools")
 # library(caTools)
 # set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
+# split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 # training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+# testing_set = subset(dataset, split == FALSE)
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#Escalado de valores
+# training_set[,2:3] = scale(training_set[,2:3])
+# testing_set[,2:3] = scale(testing_set[,2:3])
 
-# Fitting Linear Regression to the dataset
+# Ajustar el Modelo de Regresión Lineal con el Conjunto de Datos
 lin_reg = lm(formula = Salary ~ .,
              data = dataset)
 
-# Fitting Polynomial Regression to the dataset
+
+# AJustar Modelo de Regresión Polinómica con el Conjunto de Datos
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
 dataset$Level4 = dataset$Level^4
-poly_reg = lm(formula = Salary ~ .,
+poly_reg = lm(formula = Salary ~.,
               data = dataset)
 
-# Visualising the Linear Regression results
-# install.packages('ggplot2')
+# Visulización de modelo lineal
 library(ggplot2)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
+             color = "red") +
   geom_line(aes(x = dataset$Level, y = predict(lin_reg, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Linear Regression)') +
-  xlab('Level') +
-  ylab('Salary')
+            color = "blue") +
+  ggtitle("Predicción lineal del sueldo en función del nivel del empleado") + 
+  xlab("Nivel del empleado") +
+  ylab("Sueldo (en $)")
 
-# Visualising the Polynomial Regression results
-# install.packages('ggplot2')
-library(ggplot2)
-ggplot() +
-  geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
-
-# Visualising the Regression Model results (for higher resolution and smoother curve)
-# install.packages('ggplot2')
-library(ggplot2)
+# Visualización del modelo polinómico
 x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
-             colour = 'red') +
-  geom_line(aes(x = x_grid, y = predict(poly_reg,
+             color = "red") +
+  geom_line(aes(x = x_grid, y = predict(poly_reg,  
                                         newdata = data.frame(Level = x_grid,
-                                                             Level2 = x_grid^2,
-                                                             Level3 = x_grid^3,
-                                                             Level4 = x_grid^4))),
-            colour = 'blue') +
-  ggtitle('Truth or Bluff (Polynomial Regression)') +
-  xlab('Level') +
-  ylab('Salary')
+                                                            Level2 = x_grid^2,
+                                                            Level3 = x_grid^3,
+                                                            Level4 = x_grid^4))),
+            color = "blue") +
+  ggtitle("Predicción polinómica del sueldo en función del nivel del empleado") + 
+  xlab("Nivel del empleado") +
+  ylab("Sueldo (en $)")
 
-# Predicting a new result with Linear Regression
-predict(lin_reg, data.frame(Level = 6.5))
+#Predicción de nuevos resultados con Regresión Lineal
+y_pred = predict(lin_reg, newdata = data.frame(Level = 6.5))
 
-# Predicting a new result with Polynomial Regression
-predict(poly_reg, data.frame(Level = 6.5,
-                             Level2 = 6.5^2,
-                             Level3 = 6.5^3,
-                             Level4 = 6.5^4))
+
+#Predicción de nuevos resultadosde la Regresión Polinómica
+y_pred_poly = predict(poly_reg, newdata = data.frame(Level = 6.5,
+                                                Level2 = 6.5^2,
+                                                Level3 = 6.5^3,
+                                                Level4 = 6.5^4))
+
